da6176252d443dec21167961a3605408c9202293
jlirascand <jlirascand1@educacion.navarra.es>
2025-11-05 12:15:32 +0100

    Terminada la función de arrastrar la tarjeta y añadido el degradado de la imagen

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ const contadorWrapper = document.getElementsByClassName("contador")[0];
 const textoContador = document.getElementById("contador");
 let contador = 0;
 
+// Información de todos los perros -> [Nombre Archivo, Nombre perro, Edad]
 const perros = [
     ["euri.webp", "Euri", "2 años"], 
     ["ion.webp", "Ion", "2 años"], 
@@ -22,6 +23,7 @@ const perros = [
     ["txisto.webp", "Txisto", "2 años"], 
 ]
 
+// Añadimos todas las tarjetas poniendo los datos debidos
 perros.forEach(element => {
     const tarjeta = `<div class="tarjeta">
                         <div class=degradado></div>
@@ -31,22 +33,31 @@ perros.forEach(element => {
     tarjetasWrapper.innerHTML += tarjeta;
 });
 
-// botones.forEach(boton => {
-//     boton.onclick = function() {
-//         const tarjeta = tarjetas[tarjetas.length -1]
-//         tarjetasWrapper.removeChild(tarjeta);
-        
-//     }
-// });
+// Hacemos los objetos draggables
+for (let index = 0; index < tarjetas.length; index++) {
+    dragElement(tarjetas[index]);
+}
 
-botonCorazon.onclick = async function() {
+
+botonCorazon.onclick = function() {
+    // Cogemos la tarjeta que está arriba del todo
     const primeraTarjeta = tarjetas[tarjetas.length -1];
+
+    // Movemos hacia la derecha y la rotamos 20 grados y le bajamos la opacidad a 0
+    // La transición se hace con "transition" en la clase .tarjeta
     primeraTarjeta.style.transform = "translateX(50%) rotate(20deg)";
     primeraTarjeta.style.opacity = 0;
+
+    // Sumamos 1 al contador, actualizamos el texto 
     contador += 1;
     textoContador.innerText = contador;
-    contadorWrapper.style.display = "flex";
+    // Si es la primera vez le ponemos flex para que se muestre
+    if (contador == 1)
+    {
+        contadorWrapper.style.display = "flex";
+    }
 
+    // Esperamos a que termine la transición y eliminamos la tarjeta
     setTimeout(() => {
         tarjetasWrapper.removeChild(primeraTarjeta);
     }, 100);
@@ -54,10 +65,78 @@ botonCorazon.onclick = async function() {
 }
 
 botonCross.onclick = function() {
+    // Cogemos la tarjeta que está arriba del todo
     const primeraTarjeta = tarjetas[tarjetas.length -1];
+
+    // Movemos hacia la izquierda y la rotamos -20 grados y le bajamos la opacidad a 0
+    // La transición se hace con "transition" en la clase .tarjeta
     primeraTarjeta.style.transform = "translateX(-400px) rotate(-20deg)";
     primeraTarjeta.style.opacity = 0;
+
+    // Esperamos a que termine la transición y eliminamos la tarjeta
     setTimeout(() => {
         tarjetasWrapper.removeChild(primeraTarjeta);
     }, 100);
+}
+
+// Funcion para hacer el objeto arrastrable
+function dragElement(element) {
+    // Guardamos la posicón original de la tarjeta para que en caso de no llegar la distancia requerida se resetee la posición
+    const posicionOriginal = element.offsetLeft;
+    
+    // Multiplo para hacer que se mueva más al arrastrar la tarjeta
+    const sensibilidadMovmimiento = 20;
+    const sensibilidadRotacion = 10;
+    let pos1 = 0, pos3 = 0;
+    
+    // Añadir listener
+    element.addEventListener("mousedown", dragMouseDown);
+
+    function dragMouseDown(evento) {
+
+        evento.preventDefault();
+        
+        // Asignamos la posicion 3 a la posición del ratón
+        // Al ser solo la X no recogemos la posición Y
+        pos3 = evento.clientX;
+        document.onmouseup = function(){
+            evento = evento || window.event;
+            evento.preventDefault();
+            pos1 = (pos3 - evento.clientX) * sensibilidadMovmimiento;
+            pos3 = evento.clientX;
+            closeDragElement(pos1, pos3);
+        };
+        
+        // Se añade el listener aqui para que haga efecto solo cuando se mueva el ratón
+        document.addEventListener("mousemove", elementDrag);
+    }
+
+    function elementDrag(evento) {
+        
+        evento.preventDefault();
+        pos1 = (pos3 - evento.clientX) * sensibilidadMovmimiento;
+        pos3 = evento.clientX;
+        element.style.left = (element.offsetLeft - pos1) + "px";
+        
+    }
+
+    function closeDragElement(pos1, pos3) {
+        
+        // Eliminamos el mouse up y move para prevenir que se mueva o el usuario haga click durante el proceso de desvanecer la tarjeta
+        document.onmouseup = null;
+        document.onmousemove = null;
+        
+        if (Math.abs((element.offsetLeft - pos1) - pos3) < 1000 || Math.abs((pos3 - element.offsetLeft - pos1)) < 1000) {
+            element.style.left = `${posicionOriginal}px`
+            console.log("test");
+            
+        } else if (pos3 < pos1){
+            element.style.transform = "translateX(50%) rotate(20deg)";
+            element.style.opacity = 0;
+        } else {
+            element.style.transform = "translateX(-400px) rotate(-20deg)";
+            element.style.opacity = 0;
+        }
+
+    }
 }

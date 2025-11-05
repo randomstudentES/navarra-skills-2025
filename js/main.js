9264e08c8fe3fa8ea1c0796b87737801496952fe
jlirascand <jlirascand1@educacion.navarra.es>
2025-11-05 10:19:40 +0100

    Todas las tarjetas generadas por javascript y centrado

--- js/main.js
+++ js/main.js
@@ -3,13 +3,34 @@ const botonCorazon = document.getElementById("corazon");
 const botonCross = document.getElementById("cross");
 
 const botones = [resetBoton, botonCorazon, botonCross];
-const tarjeta = document.getElementsByClassName("tarjeta")[0];
+const tarjetas = document.getElementsByClassName("tarjeta");
+const tarjetasWrapper = document.getElementsByClassName("tarjetas")[0];
+
+const perros = [
+    ["euri.webp", "Euri", "2 años"], 
+    ["ion.webp", "Ion", "2 años"], 
+    ["lili.webp", "Lili", "2 años"], 
+    ["luna.webp", "Luna", "2 años"], 
+    ["rita.webp", "Rita", "2 años"], 
+    ["shiva.webp", "Shiva", "2 años"], 
+    ["thai.webp", "Thai", "2 años"], 
+    ["thor.webp", "Thor", "2 años"], 
+    ["txisto.webp", "Txisto", "2 años"], 
+]
+
+perros.forEach(element => {
+    const tarjeta = `<div class="tarjeta">
+                        <div class=degradado></div>
+                        <img src="assets/pets/${element[0]}" alt="">
+                        <p><span class="big">${element[1]}</span>, ${element[2]}</p>
+                    </div>`;
+    tarjetasWrapper.innerHTML += tarjeta;
+});
 
 botones.forEach(boton => {
     boton.onclick = function() {
-        const imagenes = tarjeta.getElementsByTagName("img")
-        const image = imagenes[imagenes.length -1]
-        tarjeta.removeChild(image);
+        const tarjeta = tarjetas[tarjetas.length -1]
+        tarjetasWrapper.removeChild(tarjeta);
     }
 });
 
